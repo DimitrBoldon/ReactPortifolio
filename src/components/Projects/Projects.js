320ce50ff5687da27df009022463d67fc0b4f313
DimitriBoldon <dimitri@tiksdd.com>
2023-11-07 19:49:02 -0600

    edited and change to code

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,3 +25,43 @@ function Projects() {
               
             />
           </Col>
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Employee tracker"
+              description="his app is a employee tracker application that allows to update a employee, roles and departments"
+              ghLink="https://github.com/DimitrBoldon/Employee-Tracker"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Book search"
+              description="Created a book search using java, react and schemas. Able to search for books in your local saves"
+              ghLink="https://github.com/DimitrBoldon/book-search"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Track"
+              description="This work being a group project done with my fellow coders, using all we learn for site track app built from the ground up"
+              ghLink="https://github.com/rgadewar/track"
+              
+            />
+          </Col>
+
+          
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
