c6207c445782a054f5f582bbce7422066f17c28c
DimitriBoldon <dimitri@tiksdd.com>
2023-11-07 19:22:09 -0600

    edited and change to code

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Not taker"
+              description="A app use to make and delete notes via usage of a site and being save locally"
+              ghLink="https://github.com/DimitrBoldon/NoteTaker"
+              
+            />
+          </Col>
